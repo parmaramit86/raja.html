8a0d0937f3a71df8e964cc885d28172ab29fb2c7
Amit Parmar <parmaramit8600@gmail.com>
2026-04-14 20:03:33 +0530

    Add files via upload

---
+++ store.php
@@ -0,0 +1,7 @@
+<?php
+$name="rahul";
+$age=32;
+echo"user details<br>";
+echo"name:$name<br>";
+echo"age:$age<br>";
+?>
---
+++ swap.php
@@ -0,0 +1,13 @@
+<?php
+$a=40;
+$b=59;
+echo"<br>before swapping<br>";
+echo"<br>a=$a<br>";
+echo"b=$b<br>";
+//logic of swapping
+$temp=$a;
+$a=$b;
+echo "<br>after swapping<br>";
+echo"a=$a<br>";
+echo"b=$b";
+?>
